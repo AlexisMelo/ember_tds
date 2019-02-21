3f0f5d6c44ba07b7a129cca12aaa15b5d2a09569
melo <21701222@etu.unicaen.fr>
2019-02-21 22:06:20 +0100

    Fin exercice 1 - notes

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -4,11 +4,21 @@ export default Controller.extend({
 
   actions:{
     save(model){
-      //let model= this.get('model');
-      // this.modelFor(this.routeName); //recuperer model actif
-
-      model.set('style','bg-success');
-      model.set('info','Note enregistrée');
+      document.cookie="note="+model.get('content');
+      model.set('info',"Note enregistrée");
+      model.set('alertVisible',true);
+      model.set('styleAlerte',"#27ae60");
       },
+
+    clear(model) {
+      model.set('content','');
+
+      //obligé de mettre un délai parceque le model.set au dessus déclenche la fonction "updateinfo" qui met l'alerte visible
+      //et apparement ça se déclenche après qu'on cache l'alerte avec le model.set en dessous
+      setTimeout(function() {
+        model.set('alertVisible',false)
+      }, 1);
+
+    }
   }
 });
--- app/routes/ex1.js
+++ app/routes/ex1.js
@@ -2,30 +2,54 @@ import Route from '@ember/routing/route';
 import EmberObject, {computed} from '@ember/object';
 
 const Note = EmberObject.extend({
-  content:'',
-  MAX: 25,
-  info : null,
-  style : 'bg-warning',
-  alertVisible : true,
-  sizeused: computed('content', function(){
+  content: '',
+  MAX: 100,
+  info: '',
+  styleAlerte: '#3498db',
+  alertVisible: false,
+  sizeused: computed('content', function () {
+    this.updateinfo();
     return this.content.length;
   }),
-  sizeleft: computed('content', function() {
-    return this.MAX-this.content.length;
+  sizeleft: computed('content', function () {
+    return this.MAX - this.content.length;
   }),
-  sizeinterdit : computed('content', function() {
-    if (this.MAX-this.content.length < 0 ){
-      return 'interdit';
+  pourcentageAvancement: computed('content', function () {
+    return (this.sizeused * 100) / this.MAX;
+  }),
+  styleBarre : computed('content', function() {
+    if (this.pourcentageAvancement < 25) {
+      return "#27ae60";
+
+    } else if (this.pourcentageAvancement < 50) {
+      return "#f1c40f";
+
+    } else if (this.pourcentageAvancement < 75) {
+      return "#e67e22";
+
+    } else {
+      return "#e74c3c";
+
     }
-    else
-    {
-      return this.MAX-this.content.length;
+  }),
+  updateinfo() {
+    if (this.content != document.cookie.split('note=')[1]) {
+      this.set('info', "Note modifiée");
+      this.set('alertVisible', true);
+      this.set('styleAlerte', "#3498db");
+
     }
-  })
+  }
 })
 
 export default Route.extend({
   model() {
+    if(document.cookie.split('note=')[1] != null)
+    {
+      return Note.create({
+        content: document.cookie.split('note=')[1]
+      });
+    }
     return new Note();
   }
 });
